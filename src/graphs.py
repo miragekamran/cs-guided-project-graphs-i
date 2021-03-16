72566decf15cb10bc64f81723082f555a72628f2
mirage <mirage.kamran.pkt@gmail.com>
2021-03-15 18:39:55 -0700

    new file added

---
+++ src/graphs.py
@@ -0,0 +1,11 @@
+# Adjacency Matrix (UAG)
+graph_matrix = [
+	[0, 1, 0, 0, 0],
+	[1, 0, 1, 1, 1],
+	[0, 1, 0, 1, 0],
+	[0, 1, 1, 0, 0],
+	[0, 1, 0, 0, 0]
+]
+
+# what are my neighbors for A?
+graph_matrix[0] # <- All the neighbors for A
